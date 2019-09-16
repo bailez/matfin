5cbb74e3579e2a02545278f9b2518863b74f80d4
bailez <bailez@usp.br>
2019-09-16 20:48:03 -0300

    Create params and add methods to simple function

--- matfin/matfin.py
+++ matfin/matfin.py
@@ -1,18 +1,46 @@
 class Capitalize:
-    def __init__(self, interest= None, payment= None, periods = None, present_value = None, future_value = None):
-        self.r = interest
-        self.p = payment
-        self.n = periods
-        self.pv = present_value
-        self.fv = future_value
 
+    def __init__(self, interest = None, periods = None, 
+                present_value = None, future_value = None):
+        
+        self.params = [interest,periods,present_value,future_value]
+    
+    def _check_params(self):
         pass
-    
-    def simple(self):
-        print('Capitalização Simples')
-    
-    def compound(self):
-        print('Capitalização Composta Discreta')
 
-    def continuous(self):
-        print('Capitalização Composta Contínua')
+    def simple(self, debug=False):
+        # S = P(1 + i * n)
+        result = None
+        r, n, pv, fv = self.params[0], self.params[1], self.params[2], self.params[3]
+        if r == None:
+            pass
+        elif n == None:
+            pass
+        elif pv == None:
+            pass
+        elif fv == None:
+            pass
+        else:
+            if debug:
+                print('')
+        if debug:
+            print('Capitalização Simples')
+        return result
+    
+    def compound(self, debug = False):
+        result = None
+
+
+        if debug:
+            print('Capitalização Composta Discreta')
+        return result
+        
+
+    def continuous(self, debug = False):
+        result = None
+
+
+        if debug:
+            print('Capitalização Composta Contínua')
+        return result
+        
