a1403e9c7188fdf8d265ddd66bb9552e329c4528
bailez <bailez@usp.br>
2019-09-16 22:05:06 -0300

    Funçao de juros simples

--- matfin/matfin.py
+++ matfin/matfin.py
@@ -5,12 +5,12 @@ class Capitalize:
         
         self.params = [interest,periods,present_value,future_value]
     
-    def _check_params(self, debug):
+    def _check_params(self):
         try:
             list(map(float,self.params))
             return True
         except ValueError:
-            raise ValueError()'A non-number argument was given')
+            raise ValueError('A non-number argument was given')
         except TypeError:
             c = 0
             for i in self.params:
@@ -19,23 +19,21 @@ class Capitalize:
             if c !=1:
                 raise TypeError('There are missing arguments')
 
-    def simple(self, debug=False):
+    def simple(self):
         # S = P(1 + i * n)
-        _check_params(debug)
+        self._check_params()
         result = None
         r, n, pv, fv = self.params[0], self.params[1], self.params[2], self.params[3]
         if r == None:
-            pass
+            result = (fv/pv - 1)/n
         elif n == None:
-            pass
+            result = (fv/pv - 1)/r
         elif pv == None:
-            pass
+            result = fv/(1 + r*n)
         elif fv == None:
-            pass
+            result = pv*(1 + r*n)
         else:
             raise TypeError('Capitalize.simple() takes 3 positional arguments but 4 were given')
-        if debug:
-            print('Capitalização Simples')
         return result
     
     def compound(self, debug = False):
--- test.py
+++ test.py
@@ -1,4 +1,6 @@
 import matfin
 
 #matfin.Capitalize().simple(debug=True)
-matfin.Capitalize().simple(debug=False)
+#matfin.Capitalize().simple(debug=False)
+x = matfin.Capitalize(interest=0.25,periods=3,future_value=250000).simple()
+print(x)
