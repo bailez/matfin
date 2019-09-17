4b32531e411261f0369b12f4152b4e2627fdce66
bailez <bailez@usp.br>
2019-09-17 15:34:03 -0300

    Change matfin.py name to interest.py

--- test.py
+++ test.py
@@ -4,10 +4,10 @@ import matfin
 #matfin.Capitalize().simple(debug=False)
 #x = matfin.Capitalize(interest=0.25,periods=3,future_value=250000).simple()
 
-cap = matfin.Capitalize
-x = cap(interest= 0.02,
-    present_value= 150000,
-    future_value= 600000,
+cap = matfin.Interest
+x = cap(present_value= 100000,
+    periods=3*12,
+    future_value=350000
     )
 
 print(x.continuous())
