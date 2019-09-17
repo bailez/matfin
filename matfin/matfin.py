d83f3de53118fa715921c3d1ee93abe94290f47a
bailez <bailez@usp.br>
2019-09-16 22:44:30 -0300

    Create _frequency() function

--- matfin/matfin.py
+++ matfin/matfin.py
@@ -1,8 +1,8 @@
+import math
 class Capitalize:
 
     def __init__(self, interest = None, periods = None, 
                 present_value = None, future_value = None):
-        
         self.params = [interest,periods,present_value,future_value]
     
     def _check_params(self):
@@ -12,17 +12,16 @@ class Capitalize:
         except ValueError:
             raise ValueError('A non-number argument was given')
         except TypeError:
-            c = 0
-            for i in self.params:
-                if i == None:
-                    c += 1
-            if c !=1:
+            none_count = sum(x is None for x in self.params)
+            if none_count !=1:
                 raise TypeError('There are missing arguments')
+    
+    def _frequency(self,ifreq,nfreq, r):
+        return ''
 
     def simple(self):
-        # S = P(1 + i * n)
+        #fv = pv(1 + r*n)
         self._check_params()
-        result = None
         r, n, pv, fv = self.params[0], self.params[1], self.params[2], self.params[3]
         if r == None:
             result = (fv/pv - 1)/n
@@ -35,20 +34,26 @@ class Capitalize:
         else:
             raise TypeError('Capitalize.simple() takes 3 positional arguments but 4 were given')
         return result
-    
-    def compound(self, debug = False):
-        result = None
 
-
-        if debug:
-            print('Capitalização Composta Discreta')
+    def compound(self,interest_freq = 'Y', period_freq = 'M'):
+        #pv = fv/(1 + r)^n 
+        self._check_params()
+        r, n, pv, fv = self.params[0], self.params[1], self.params[2], self.params[3]
+        r = self._frequency(interest_freq,period_freq,r)
+        if r == None:
+            result = (fv/pv - 1)/n
+        elif n == None:
+            result = (fv/pv - 1)/r
+        elif pv == None:
+            result = fv/((1 + r)**n)
+        elif fv == None:
+            result = pv*((1 + r)**n)
+        else:
+            raise TypeError('Capitalize.compound() takes 3 positional arguments but 4 were given')       
         return result
-        
 
     def continuous(self, debug = False):
         result = None
-
-
         if debug:
             print('Capitalização Composta Contínua')
         return result
--- test.py
+++ test.py
@@ -2,5 +2,6 @@ import matfin
 
 #matfin.Capitalize().simple(debug=True)
 #matfin.Capitalize().simple(debug=False)
-x = matfin.Capitalize(interest=0.25,periods=3,future_value=250000).simple()
-print(x)
+#x = matfin.Capitalize(interest=0.25,periods=3,future_value=250000).simple()
+y = matfin.Capitalize(interest=(0.12)/7,periods=7, present_value=100).compound()
+print(y)
